37e3498bfffe72d57e0a4a7f1e98d485acf6de53
Bijan Kundu <bijankundu025@gmail.com>
2022-05-03 21:32:15 +0530

    configured react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <ChakraProvider>
-      <Layout>
-        <div>Hello</div>
-      </Layout>
+      <Routes>
+        {publicRoutes.map(({ Component, Layout, path }, idx) => {
+          return (
+            <Route
+              key={idx}
+              element={
+                <Layout>
+                  <Component />
+                </Layout>
+              }
+              path={path}
+            />
+          );
+        })}
+      </Routes>
     </ChakraProvider>
   );
 };
+
+const publicRoutes = [
+  {
+    path: "/",
+    Component: Home,
+    Layout: Layout,
+  },
+];
 
 export default App;
