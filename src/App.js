a4fc994764c85db42f6a4231ae039bfc9d9d022d
Bijan Kundu <bijankundu025@gmail.com>
2022-05-09 08:54:29 +0530

    added custom theme color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#eeeefb",
+      200: "#ebeafa",
+      300: "#c2c1f1",
+      400: "#9997e8",
+      500: "#706dde",
+      600: "#4744d5",
+      700: "#332fd0",
+      800: "#2e2abb",
+      900: "#2926a6",
+    },
+  },
+});
+
 const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Routes>
         {publicRoutes.map(({ Component, Layout, path }, idx) => {
           return (
