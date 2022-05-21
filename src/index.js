b34858e0dc77007785b196fac7376c4e3e7427a5
Soham Sadhu <46419166+samsad01@users.noreply.github.com>
2022-05-21 13:04:23 +0530

    Changed to Soham's version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from "./App";
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+    
   </React.StrictMode>
 );
+
+
