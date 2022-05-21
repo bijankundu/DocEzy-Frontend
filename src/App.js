10eb943d285f5b5f3ab0b2cf023fccb8a9ce8c38
Soham Sadhu <46419166+samsad01@users.noreply.github.com>
2022-05-21 13:01:58 +0530

    Changed to my version

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
-import React from "react";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react'
 
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Body from './Components/Body'
 
-const theme = extendTheme({
-  colors: {
-    brand: {
-      100: "#eeeefb",
-      200: "#ebeafa",
-      300: "#c2c1f1",
-      400: "#9997e8",
-      500: "#706dde",
-      600: "#4744d5",
-      700: "#332fd0",
-      800: "#2e2abb",
-      900: "#2926a6",
-    },
-  },
-});
-
-const App = () => {
+export default function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Routes>
-        {publicRoutes.map(({ Component, Layout, path }, idx) => {
-          return (
-            <Route
-              key={idx}
-              element={
-                <Layout>
-                  <Component />
-                </Layout>
-              }
-              path={path}
-            />
-          );
-        })}
-      </Routes>
-    </ChakraProvider>
-  );
-};
-
-const publicRoutes = [
-  {
-    path: "/",
-    Component: Home,
-    Layout: Layout,
-  },
-];
-
-export default App;
+    <div>
+      <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
+    </div>
+  )
+}
