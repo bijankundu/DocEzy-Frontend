c7a40e6f5ab70855c892fee9a23057754ca2d14d
Bijan Kundu <bijankundu025@gmail.com>
2022-05-21 19:45:03 +0530

    fixed framer motion error

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
-import { ReactNode } from 'react';
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { ReactNode } from "react";
 import {
   Box,
   Container,
@@ -11,41 +11,38 @@ import {
   VisuallyHidden,
   chakra,
   useColorModeValue,
-} from '@chakra-ui/react';
-import { FaTwitter, FaYoutube, FaInstagram, FaLinkedin, FaFacebook } from 'react-icons/fa';
+} from "@chakra-ui/react";
+import { FaTwitter, FaYoutube, FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 //import AppStoreBadge from '@/components/AppStoreBadge';
 //import PlayStoreBadge from '@/components/PlayStoreBadge';
 
 const ListHeader = ({ children }) => {
   return (
-    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
       {children}
     </Text>
   );
 };
 
-const SocialButton = ({
-  children,
-  label,
-  href,
-} ) => {
+const SocialButton = ({ children, label, href }) => {
   return (
     <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
       w={8}
       h={8}
-      cursor={'pointer'}
-      as={'a'}
+      cursor={"pointer"}
+      as={"a"}
       href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
       _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
       <VisuallyHidden>{label}</VisuallyHidden>
       {children}
     </chakra.button>
@@ -54,73 +51,67 @@ const SocialButton = ({
 
 export default function LargeWithAppLinksAndSocial() {
   return (
-    <Box
-      bg={'purple.100'}
-      
-      color={useColorModeValue('gray.700', 'gray.200')}>
-      <Container as={Stack} maxW={'6xl'} py={10}>
+    <Box bg={"purple.100"} color={useColorModeValue("gray.700", "gray.200")}>
+      <Container as={Stack} maxW={"6xl"} py={10}>
         <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
-          <Stack align={'flex-start'}>
+          <Stack align={"flex-start"}>
             <ListHeader>Company</ListHeader>
-            <Link href={'#'}>About Us</Link>
-            <Link href={'#'}>Blog</Link>
-            <Link href={'#'}>Careers</Link>
-            <Link href={'#'}>Contact Us</Link>
+            <Link href={"#"}>About Us</Link>
+            <Link href={"#"}>Blog</Link>
+            <Link href={"#"}>Careers</Link>
+            <Link href={"#"}>Contact Us</Link>
           </Stack>
 
-          <Stack align={'flex-start'}>
+          <Stack align={"flex-start"}>
             <ListHeader>Support</ListHeader>
-            <Link href={'#'}>Help Center</Link>
-            <Link href={'#'}>Safety Center</Link>
-            <Link href={'#'}>Community Guidelines</Link>
+            <Link href={"#"}>Help Center</Link>
+            <Link href={"#"}>Safety Center</Link>
+            <Link href={"#"}>Community Guidelines</Link>
           </Stack>
 
-          <Stack align={'flex-start'}>
+          <Stack align={"flex-start"}>
             <ListHeader>Legal</ListHeader>
-            <Link href={'#'}>Cookies Policy</Link>
-            <Link href={'#'}>Privacy Policy</Link>
-            <Link href={'#'}>Terms of Service</Link>
-            <Link href={'#'}>Law Enforcement</Link>
+            <Link href={"#"}>Cookies Policy</Link>
+            <Link href={"#"}>Privacy Policy</Link>
+            <Link href={"#"}>Terms of Service</Link>
+            <Link href={"#"}>Law Enforcement</Link>
           </Stack>
 
-          <Stack align={'flex-start'}>
+          <Stack align={"flex-start"}>
             <ListHeader>Install App</ListHeader>
-            <Link href={'#'}>Play Store</Link>
-            <Link href={'#'}>App Store</Link>
+            <Link href={"#"}>Play Store</Link>
+            <Link href={"#"}>App Store</Link>
           </Stack>
         </SimpleGrid>
       </Container>
 
-      <Box
-        borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+      <Box borderTopWidth={1} borderStyle={"solid"} borderColor={useColorModeValue("gray.200", "gray.700")}>
         <Container
           as={Stack}
-          maxW={'6xl'}
+          maxW={"6xl"}
           py={4}
-          direction={{ base: 'column', md: 'row' }}
+          direction={{ base: "column", md: "row" }}
           spacing={4}
-          justify={{ md: 'space-between' }}
-          align={{ md: 'center' }}>
+          justify={{ md: "space-between" }}
+          align={{ md: "center" }}
+        >
           <Text>© 2022 DocEzy. All rights reserved</Text>
-          <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'Twitter'} href={'#'}>
+          <Stack direction={"row"} spacing={6}>
+            <SocialButton label={"Twitter"} href={"#"}>
               <FaTwitter />
             </SocialButton>
-            <SocialButton label={'YouTube'} href={'#'}>
+            <SocialButton label={"YouTube"} href={"#"}>
               <FaYoutube />
             </SocialButton>
-            <SocialButton label={'Instagram'} href={'#'}>
+            <SocialButton label={"Instagram"} href={"#"}>
               <FaInstagram />
             </SocialButton>
-            <SocialButton label={'Linkedin'} href={'#'}>
+            <SocialButton label={"Linkedin"} href={"#"}>
               <FaLinkedin />
             </SocialButton>
-            <SocialButton label={'Facebook'} href={'#'}>
+            <SocialButton label={"Facebook"} href={"#"}>
               <FaFacebook />
             </SocialButton>
-           
           </Stack>
         </Container>
       </Box>
